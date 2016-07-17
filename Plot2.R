74e9b9169c4ed7deba52dfe0a1658a2091f1987b
St John Kelliher <stjmk63@gmail.com>
2016-07-17 16:29:54 -0400

    Add files via upload

---
+++ Plot2.R
@@ -0,0 +1,21 @@
+library(lubridate)
+
+setwd("Code/ExploratoryDataAnalysis/WeekOne")
+URL <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+
+# Windows
+download.file(URL, destfile="household_power_consumption.zip")
+
+# Other OS
+# download.file(URL, destfile="household_power_consumption.zip", method="curl")
+
+unzip("household_power_consumption.zip")      
+PowerData <- read.table("household_power_consumption.txt", header = TRUE, sep = ";")
+PowerDataXtract <- PowerData[as.character(PowerData$Date) %in% c("1/2/2007", "2/2/2007"),]
+DateTime <- paste(PowerDataXtract$Date, PowerDataXtract$Time)
+DateTime <- dmy_hms(DateTime)
+PowerDataXtract <- cbind(PowerDataXtract, DateTime)
+
+png("plot2.png", width=480, height=480, units="px")
+with(PowerDataXtract, plot(DateTime, as.numeric(as.character(Global_active_power)), type="l", xlab="", ylab="Global Active Power (kilowatts)"))
+dev.off()
